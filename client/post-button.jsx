509391d7358c7e5d5ed17f725067f80e6cb465a7
Dean Larbi <dlarbi@dlarbimac.local>
2017-12-26 14:04:33 -0800

    latest changes

--- client/post-button.jsx
+++ client/post-button.jsx
@@ -23,29 +23,29 @@ export default class PostButton extends React.Component {
 		this.setState( { disabled: true } )
 		withTimeout(
 
-			uploadWordpressMediaFromUrl(this.props.site.blog_id, this.props.selectedImageUrl)
-				.then((result) => {
-					console.log('Uploaded new media to wordpress', result);
-					postToWordPress(this.props.site.blog_id, {
-						categories: this.props.postCategories,
-						tags: this.props.postTags,
-						type: this.props.postType,
-						featured_image: result.media[0].ID
-					})
-						.then( ( post ) => {
-							console.log('POST CREATED', post)
-							this.setState( { disabled: false } )
-							this.props.onPostSave( post  )
-						} )
-						.catch( ( e ) => {
-							this.props.errorHandler( e )
-							this.setState( { disabled: false } )
-						})
+			// uploadWordpressMediaFromUrl(this.props.site.blog_id, this.props.selectedImageUrl)
+			// 	.then((result) => {
+			// 		console.log('Uploaded new media to wordpress', result);
+      //
+			// 	})
+			// 	.catch((err) => {
+			// 		console.log('Error uploading new media to wordpress', err);
+			// 	})
+			postToWordPress(this.props.site.blog_id, {
+				categories: this.props.postCategories,
+				tags: this.props.postTags,
+				type: this.props.postType,
+				// featured_image: result.media[0].ID
+			})
+				.then( ( post ) => {
+					console.log('POST CREATED', post)
+					this.setState( { disabled: false } )
+					this.props.onPostSave( post  )
+				} )
+				.catch( ( e ) => {
+					this.props.errorHandler( e )
+					this.setState( { disabled: false } )
 				})
-				.catch((err) => {
-					console.log('Error uploading new media to wordpress', err);
-				})
-
 
 		)
 	}
