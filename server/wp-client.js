06b35958c05ed6cd98fcc7c827416fb689ee0f82
Dean Larbi <dlarbi@dlarbimac.local>
2018-01-12 10:52:17 -0600

    uses unsplash text

--- server/wp-client.js
+++ server/wp-client.js
@@ -189,7 +189,7 @@ export function WPClient( PropertiesService, UrlFetchApp ) {
 				 "author": post.author.ID,
 				 "modified": post.modified,
 			   "content":`[caption id=\"attachment_${attachmentId}\" align=\"alignnone\" width=\"${imageData.width}\"]<img class=\"wp-image-${attachmentId} size-full_bleed\" src=\"
-				 ${imageData.URL}?w=${imageData.width}\" alt=\"${photoDescription}\" width=\"${imageData.width}\" height=\"${imageData.height}\" /> <a href=\"${photographerUrl}?utm_source=thought_catalog_wordpress_plugin&utm_medium=referral\">${photographerName}</a>[/caption]${post.content}`,
+				 ${imageData.URL}?w=${imageData.width}\" alt=\"${photoDescription}\" width=\"${imageData.width}\" height=\"${imageData.height}\" /> <a href=\"${photographerUrl}?utm_source=thought_catalog_wordpress_plugin&utm_medium=referral\">${photographerName} / Unsplash</a>[/caption]${post.content}`,
 			}
 		};
 		const response = UrlFetchApp.fetch(
